8245339a8c0d331285a6a042273ed8e73d9cdd4a
Brandon Moretz <bmoretz82@gmail.com>
2021-09-16 22:05:23 -0400

    learning go, ch1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bmoretz/learninggo
+
+go 1.16
---
+++ hello/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bmoretz/learninggo/morestrings"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG, OlleH"))
+}
+
+func Hello() string {
+	return "Hello, world."
+}
